d00d1dc035aa01b126560057e005b2dd1c120742
pedrohzwang <pedroh.zwang@gmail.com>
2021-10-03 23:42:17 -0300

    first commit

---
+++ src/main/java/com/gracehopper/gracehoppermonitorbackend/GraceHopperMonitorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gracehopper.gracehoppermonitorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraceHopperMonitorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraceHopperMonitorBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gracehopper/gracehoppermonitorbackend/GraceHopperMonitorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gracehopper.gracehoppermonitorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraceHopperMonitorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
