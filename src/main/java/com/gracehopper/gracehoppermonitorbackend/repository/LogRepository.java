2b5e75a36c0ccf2e90e29d3d3b1affd89b9a683d
Pedro <pedroh.zwang@gmail.com>
2021-12-14 23:02:22 -0300

    geracao de apis de log

--- src/main/java/com/gracehopper/gracehoppermonitorbackend/repository/LogRepository.java
+++ src/main/java/com/gracehopper/gracehoppermonitorbackend/repository/LogRepository.java
@@ -17,8 +17,8 @@ public interface LogRepository extends JpaRepository<Log, Integer> {
     @Query(value = "SELECT * FROM log WHERE id_usuario = ?1", nativeQuery = true)
     List<Log> getLogsByUser(Integer userId);
 
-    @Query(value = "SELECT * FROM log WHERE id_usuario = ?1 and DATE_FORMAT(dt_log, \"%Y-%m-%d\") = \'' ?2 \''", nativeQuery = true)
-    List<Log> getLogsByUserDate(Integer userId, @Param("logDate") String logDate);
+    @Query(value = "SELECT * FROM log WHERE id_usuario = :userId and DATE_FORMAT(dt_log, \"%Y-%m-%d\") like :logDate", nativeQuery = true)
+    List<Log> getLogsByUserDate(@Param("userId") Integer userId, @Param("logDate") String logDate);
 
     @Query(value = "SELECT * FROM log WHERE id_usuario = ?1 " +
             "ORDER BY cd_log DESC, dt_log DESC LIMIT 2", nativeQuery = true)
